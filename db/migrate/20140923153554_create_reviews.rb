776a86cefb29b016fa092670c595995185b3864a
Alex Cornford <amc42294@gmail.com>
2014-09-23 11:50:40 -0400

    Added schea and things

--- db/migrate/20140923153554_create_reviews.rb
+++ db/migrate/20140923153554_create_reviews.rb
@@ -1,6 +1,12 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
+      t.integer :rating, null: false
+      t.integer :site_id, null: false
+      t.integer :user_id, null: false
+      t.integer :up_votes
+      t.integer :down_votes
+      t.text  :body
 
       t.timestamps
     end
