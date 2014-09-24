db2546701ce4356908453e19b20f7caec8f03413
Eric Kelly <heroiceric@gmail.com>
2014-09-24 15:05:21 -0400

    Test hound

--- app/models/site.rb
+++ app/models/site.rb
@@ -4,4 +4,10 @@ class Site < ActiveRecord::Base
   validates :title, uniqueness: true
   validates :url, uniqueness: true
   validates :title, :url, :description, :user_id, presence: true
+
+     validates :title, presence: true # indent fail
+
+  def hellow
+    'foo' + "bar"
+  end
 end
