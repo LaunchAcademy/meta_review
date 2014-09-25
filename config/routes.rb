c098b613ac657de2090a78d91c8e10d797b0bcbc
Peter <monroepe@gmail.com>
2014-09-25 11:30:55 -0400

    Limited reviews resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   resources :sites do
-    resources :reviews
+    resources :reviews, only: [:new, :create, :edit, :update, :destroy]
   end
 end
