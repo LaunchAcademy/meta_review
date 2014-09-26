90c02fe8569f66304a695d7c7dbed1b85ef2bdb2
Dennis Gokman <dennis.gokman@gmail.com>
2014-09-26 15:26:40 -0400

    Only configure fog in dev/production

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -4,15 +4,6 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
-  if Rails.env.production? || Rails.env.development?
-    storage :fog
-  else
-    storage :file
-  end
-
-  # storage :fog
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
