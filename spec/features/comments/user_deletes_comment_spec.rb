2115af97a61125332076809beedb80c734cd2631
Peter <monroepe@gmail.com>
2014-09-26 15:41:38 -0400

    Added test coverage for edit and delete

---
+++ spec/features/comments/user_deletes_comment_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+feature "Delete comment" do
+  scenario "user delets site succesfully" do
+    comment = FactoryGirl.create(:comment)
+    site = comment.review.site
+    sign_in_as(comment.user)
+
+    visit site_path(site)
+
+    click_on "Delete"
+
+    expect(page).to have_content "Comment destroyed successfully!"
+  end
+end
