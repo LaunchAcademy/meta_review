542c4a0275892d6cbcaa7051546968d253c25be3
Peter <monroepe@gmail.com>
2014-09-29 15:03:41 -0400

    Revert "Fixing database"
    This reverts commit e76a46242ffa6804cee28c1d9ab12f572dbfac89.

--- db/migrate/20140929155750_add_username_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddUsernameToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :username, :string, null: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140929155750) do
+ActiveRecord::Schema.define(version: 20140925190614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,11 +58,12 @@ ActiveRecord::Schema.define(version: 20140929155750) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin",                  default: false, null: false
     t.string   "username",                               null: false
+    t.boolean  "admin",                  default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
