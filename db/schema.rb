8266fd9bc669e5b4f7b50879b733b72ccd6147f3
Peter <monroepe@gmail.com>
2014-09-30 13:49:24 -0400

    Added index

--- db/schema.rb
+++ db/schema.rb
@@ -63,5 +63,6 @@ ActiveRecord::Schema.define(version: 20140930173333) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
