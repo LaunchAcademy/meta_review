d0407bf0dbf8dd241e50b6dbc520c7f6594300ea
Peter <monroepe@gmail.com>
2014-09-30 10:41:10 -0400

    Fixed tests

--- spec/mailers/review_comment_spec.rb
+++ spec/mailers/review_comment_spec.rb
@@ -1,17 +1,20 @@
 require "rails_helper"
 
-RSpec.describe ReviewComment, :type => :mailer do
+RSpec.describe ReviewComment, type: :mailer do
   describe "comment" do
-    let(:mail) { ReviewComment.comment }
+    let(:comment) { FactoryGirl.create(:comment) }
+    let(:mail) { ReviewComment.comment(comment) }
+    let(:review) { comment.review }
+    let(:recipient) { review.user }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Someone commented on your review!")
-      expect(mail.to).to eq(["to@example.org"])
+      expect(mail.to).to eq([recipient.email])
       expect(mail.from).to eq(["admin@meta-review.herokuapp.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match(comment.body)
     end
   end
 
