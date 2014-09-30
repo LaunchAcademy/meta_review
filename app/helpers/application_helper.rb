5dfc0e40802c037dfe6512c40fff73827a9799d8
Dennis Gokman <dennis.gokman@gmail.com>
2014-09-30 13:18:30 -0400

    added helper method for image logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def screenshot_for_site(site)
+    unless stw_show_url(site.url) =~ /</
+      link_to site_path(site), id: "screenshot-link" do
+        stw_show(site.url)
+      end
+    end
+  end
 end
