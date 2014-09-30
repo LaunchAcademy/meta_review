32f8d4e02be9f651588b97b7436b49e1b9eabe9b
Peter <monroepe@gmail.com>
2014-09-30 10:10:45 -0400

    Mailer working

--- spec/mailers/review_comment_spec.rb
+++ spec/mailers/review_comment_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe ReviewComment, :type => :mailer do
     let(:mail) { ReviewComment.comment }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Comment")
+      expect(mail.subject).to eq("Someone commented on your review!")
       expect(mail.to).to eq(["to@example.org"])
       expect(mail.from).to eq(["from@example.com"])
     end
