1309812026b7d9522ada1d2902df868aa322e1a4
Peter <monroepe@gmail.com>
2014-09-30 11:03:53 -0400

    Fixed hound problems

--- app/mailers/review_comment.rb
+++ app/mailers/review_comment.rb
@@ -1,18 +1,12 @@
 class ReviewComment < ActionMailer::Base
   default from: "admin@meta-review.herokuapp.com"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.review_comment.comment.subject
-  #
   def comment(comment)
     @comment = comment
     @user = @comment.review.user
     @url = "http://meta-review.herokuapp.com/site/#{@comment.review.id}"
 
     mail to: @user.email,
-      subject: "Someone commented on your review!"
+         subject: "Someone commented on your review!"
   end
 end
-
