ae965198e044b2cbe4fec903e5ddd6dcde6a08fc
Dennis Gokman <dennis.gokman@gmail.com>
2014-10-01 15:29:48 -0400

    added default avatar logic

--- db/schema.rb
+++ db/schema.rb
@@ -58,12 +58,13 @@ ActiveRecord::Schema.define(version: 20141001141637) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "username",                            null: false
     t.string   "avatar"
+    t.string   "username",                            null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   create_table "votes", force: true do |t|
     t.integer  "user_id",                null: false
