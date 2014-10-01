59ebe16ff14bcb328b1c46a8e2ea7d625ec5dcae
Dennis Gokman <dennis.gokman@gmail.com>
2014-10-01 11:10:51 -0400

    added presence validation for user and review

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,6 @@
 class Vote < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :review
+  belongs_to :user, presence: true
+  belongs_to :review, presence: true
 
   validates :value, presence: true, inclusion: { in: [1, 0, -1] }
 end
