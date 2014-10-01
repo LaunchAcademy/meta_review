dc6a17b7c2c0683fa72290192ffaa8d7f5e7f652
Peter <monroepe@gmail.com>
2014-10-01 10:34:01 -0400

    Fixed failing tests

--- spec/features/user/user_signs_up_spec.rb
+++ spec/features/user/user_signs_up_spec.rb
@@ -11,15 +11,18 @@ feature "User signs up" do
     fill_in "Password", with: "password123"
     fill_in "Password confirmation", with: "password123"
 
-    click_on "Sign up"
-
+    within("#new_user") do
+      click_on "Sign up"
+    end
     expect(page).to have_content "Welcome! You have signed up successfully."
   end
 
   scenario "user signs up with invalid info" do
     visit new_user_registration_path
 
-    click_on "Sign up"
+    within("#new_user") do
+      click_on "Sign up"
+    end
 
     expect(page).to have_content "errors prohibited this user from being saved"
   end
