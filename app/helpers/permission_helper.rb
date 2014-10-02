8f1ade40a866b6170cd61cc133c6fd9d46efebad
Nicholas Sturgis <nsturgis58@gmail.com>
2014-10-02 17:15:47 -0400

    everything merged

--- app/helpers/permission_helper.rb
+++ app/helpers/permission_helper.rb
@@ -1,6 +1,6 @@
 module PermissionHelper
   def user_has_permision_to_change?(view)
-    if current_user == view.user || current_user.admin?
+    if current_user != view.user || current_user.nil? || !current_user.admin?
       true
     else
       false
