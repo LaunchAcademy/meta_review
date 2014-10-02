ba4c9b57165cd9e93e6b7d64baba76f8c43d68ac
Alex Cornford <amc42294@gmail.com>
2014-10-02 14:23:45 -0400

    Changed the buttons

--- spec/features/votes_spec.rb
+++ spec/features/votes_spec.rb
@@ -1,20 +1,20 @@
 require "rails_helper"
 
 feature "Authenticated user can vote" do
-  scenario "User upvotes and inreases the votes by 1" do
+  scenario "User Likes and inreases the votes by 1" do
     review = FactoryGirl.create(:review)
     sign_in_as(review.user)
 
     visit site_path(review.site)
-    click_on "Upvote"
-    expect(page).to have_content "Likes: 1"
+    click_on "Like"
+    expect(page).to have_content "Like: 1"
   end
-  scenario "User downvotes and decreases the votes by 1" do
+  scenario "User DisLike and decreases the votes by 1" do
     review = FactoryGirl.create(:review)
     sign_in_as(review.user)
 
     visit site_path(review.site)
-    click_on "Downvote"
-    expect(page).to have_content "Likes: -1"
+    click_on "Dislike"
+    expect(page).to have_content "Like: -1"
   end
 end
