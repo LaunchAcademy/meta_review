18bb08c4ae5d218ac7fd1c679b461a353c96361f
Eric Kelly <heroiceric@gmail.com>
2014-10-02 21:07:17 -0400

    Update permission_helper.rb

--- app/helpers/permission_helper.rb
+++ app/helpers/permission_helper.rb
@@ -1,9 +1,5 @@
 module PermissionHelper
   def user_has_permision_to_change?(model)
-    if !current_user.nil? && current_user == model.user || !current_user.nil? && current_user.admin?
-      true
-    else
-      false
-    end
+    current_user && (current_user == model.user || current_user.admin?)
   end
 end
